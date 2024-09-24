1880d49010a39b4a9fd47695a057d5b7f388f866
Crypt06545 <mehedihasansagor2005@gmail.com>
2024-09-24 08:33:35 +0600

    add selectors

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const calcutaleBtn = document.getElementById("calculate");
+const incomeInput = document.getElementById("income");
+const coursesInput = document.getElementById("courses");
+const internetInput = document.getElementById("internet");
+calcutaleBtn.addEventListener("click", function () {});
