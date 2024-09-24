1c6ad4f3a2256a93c4f55f26fd910b6e4f124582
Crypt06545 <mehedihasansagor2005@gmail.com>
2024-09-24 18:40:42 +0600

    add calculate btn click event

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
 const calcutaleBtn = document.getElementById("calculate");
-const incomeInput = document.getElementById("income");
-const coursesInput = document.getElementById("courses");
-const internetInput = document.getElementById("internet");
-calcutaleBtn.addEventListener("click", function () {});
+
+// calculate Btn click event
+calcutaleBtn.addEventListener("click", function () {
+  const incomeInput = parseFloat(document.getElementById("income").value);
+  const softwareInput = parseFloat(document.getElementById("software").value);
+  const coursesInput = parseFloat(document.getElementById("courses").value);
+  const internetInput = parseFloat(document.getElementById("internet").value);
+  const savingsInput = parseFloat(document.getElementById("savings").value);
+  const totalcost = softwareInput + coursesInput + internetInput;
+
+  // calculate total cost
+  const totalExpenses = document.getElementById("total-expenses");
+  totalExpenses.innerText = totalcost.toFixed(2);
+
+  // Calculate remaining balance
+  const remainingBalanceEl = document.getElementById("balance");
+  const remainingBalance = incomeInput - totalcost;
+  remainingBalanceEl.innerText = remainingBalance.toFixed(2);
+
+  const savingBalaceEl = document.getElementById("savings-amount");
+  const savingBalace = (remainingBalance * savingsInput) / 100;
+  savingBalaceEl.innerText = savingBalace.toFixed(2);
+});
+
+
+// savings Btn click event
+
+
